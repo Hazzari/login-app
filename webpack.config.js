ecfe1a376d0eb7346c16135e8f022868f2b730a5
Aleksan <Dev.aleksan@gmail.com>
2021-06-24 13:58:04 +0300

    build: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+module.exports = {
+    // Главный файл
+    entry: {
+        polyfill: 'babel-polyfill',
+        app: './js/app.js',
+    },
+    // Исходники - context:
+    context: path.resolve(__dirname, 'src'),
+    // Сервер разработки
+    devServer: {
+        publicPath: '/',
+        port: 9000,
+        contentBase: path.join(process.cwd(), 'dist'),
+        host: 'localhost',
+        historyApiFallback: true,
+        noInfo: false,
+        stats: 'minimal',
+        hot: true,
+    },
+    module: {
+        // Настройка трансформации файла
+        // Для того, чтобы трансформировать файл, используются специальные утилиты - загрузчики (loaders).
+        // Массив rules  внутри объекта module определяет список правил для загрузчиков.
+        rules: [
+            {
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+                test: /\.js$/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+
+                        options: {
+                            importLoaders: 1,
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [precss, autoprefixer],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    // Вебпак плагины используются для настройки процесса сборки.
+    // Например, плагин для минификации кода (во время сборки код подвергается очистке и минификации).
+    // Или плагин для сборки html страницы и css кода (скрипты вставляются в html, куски css собираются в один файл).
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({filename: './style.css'}),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+    ],
+    // Кроме entry, мы можем указать поле, куда (в какой файл) собирать конечный результат.
+    // Это свойство задаётся с помощью поля output.
+    // По умолчанию, весь результирующий код собирается в папку dist.
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].js',
+    },
+    mode: 'development',
+};
